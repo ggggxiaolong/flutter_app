04ba19e2d02644ba541a977e14a4ce5eda27922d
mrtan <freemrtan@gmail.com>
2018-03-08 16:39:08 +0800

    Buidding Layoutts in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,112 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  @override Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcom to Flutter',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
+  @override
+  Widget build(BuildContext context) {
+    final Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text(
+                    'Oeschinen Lake campground',
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                new Text(
+                  'KanderSteg, Switzerlang',
+                  style: new TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41'),
+        ],
       ),
-      home: new RandomWords(),
     );
-  }
-}
+    
+    final Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColum(context, Icons.call, 'CALL'),
+          _buildButtonColum(context, Icons.near_me, 'ROUTE'),
+          _buildButtonColum(context, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
 
-class RandomWords extends StatefulWidget {
-  @override
-    createState() => new RandomWordsState();
-}
+    final textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+        Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _save = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Startup Name Generator'),
-          actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved,)
+          title: new Text('Top Lakes'),
+        ),
+        body: new ListView(
+          children: <Widget>[
+            new Image.asset(
+              'images/lake.jpg',
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
-        body: _buildSuggesions(),
-      );
-    }
-  
-  Widget _buildSuggesions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
-        if (i.isOdd) return new Divider(); //奇数行 返回分割线
-        final index = i ~/ 2;// i ～/ 2 的意思是 i/2
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
+      ),
     );
   }
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _save.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: (){
-        setState((){
-          if (alreadySaved) {
-            _save.remove(pair);
-          } else {
-            _save.add(pair);
-          }
-        });
-      },
-    );
-  }
+  Column _buildButtonColum(BuildContext context, IconData icon, String label){
+    final Color color = Theme.of(context).primaryColor;
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _save.map(
-            (pair){
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            }
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Icon(icon, color: color,),
+        new Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: new Text(
+            label,
+            style: new TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: color,
             ),
-            body: new ListView(children: divided,),
-          );
-        }
-      )
+          ),
+        )
+      ],
     );
   }
 }
