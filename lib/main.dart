e9987608b180c0783dab9a428bdcbf51b2c0a764
mrtan <freemrtan@gmail.com>
2018-03-13 15:51:52 +0800

    widget manage its own state

--- lib/main.dart
+++ lib/main.dart
@@ -5,148 +5,51 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Widget titleSection = new Container(
-      padding: const EdgeInsets.all(32.0),
-      child: new Row(
-        children: <Widget>[
-          new Expanded(
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: new Text(
-                    'Oeschinen Lake campground',
-                    style: new TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                new Text(
-                  'KanderSteg, Switzerlang',
-                  style: new TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          new FavoriteWidget(),
-        ],
-      ),
-    );
-    
-    final Widget buttonSection = new Container(
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          _buildButtonColum(context, Icons.call, 'CALL'),
-          _buildButtonColum(context, Icons.near_me, 'ROUTE'),
-          _buildButtonColum(context, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
-    final textSection = new Container(
-      padding: const EdgeInsets.all(32.0),
-      child: new Text(
-        '''
-        Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
-        ''',
-        softWrap: true,
-      ),
-    );
-
     return new MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Top Lakes'),
         ),
-        body: new ListView(
-          children: <Widget>[
-            new Image.asset(
-              'images/lake.jpg',
-              width: 600.0,
-              height: 240.0,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
+        body: new Center(
+          child: new TapboxA(),
         ),
       ),
     );
   }
-
-  Column _buildButtonColum(BuildContext context, IconData icon, String label){
-    final Color color = Theme.of(context).primaryColor;
-
-    return new Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new Icon(icon, color: color,),
-        new Container(
-          margin: const EdgeInsets.only(top: 8.0),
-          child: new Text(
-            label,
-            style: new TextStyle(
-              fontSize: 12.0,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        )
-      ],
-    );
-  }
 }
 
-class FavoriteWidget extends StatefulWidget {
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
   @override
-    _FavoriteWidgetState createState() => new _FavoriteWidgetState();
+    _TapboxState createState() => new _TapboxState();
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favotiteCount = 41;
+class _TapboxState extends State<TapboxA>                   {
+  bool _active = false;
 
-  void _toggleFavorite() {
+  void _handleTap() {
     setState(() {
-      if (_isFavorited) {
-        _favotiteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favotiteCount += 1;
-        _isFavorited = true;
-      }
+      _active = !_active;
     });
   }
   @override
     Widget build(BuildContext context) {
-      return new Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-         new Container(
-           padding: new EdgeInsets.all(0.0),
-           child: new IconButton(
-             icon: (_isFavorited? new Icon(Icons.star):new Icon(Icons.star_border)),
-             color: Colors.red[500],
-             onPressed: _toggleFavorite,
-           ),
-         ),
-         new SizedBox(
-           width: 18.0,
-           child: new Container(
-             child: new Text('$_favotiteCount'),
-           ),
-         )
-        ],
+      return new GestureDetector(
+        onTap: _handleTap,
+        child: new Container(
+          child: new Center(
+            child: new Text(
+              _active ? 'Active' : 'Inactive',
+              style: new TextStyle(fontSize:  32.0, color: Colors.white),
+            ),
+          ),
+          width: 200.0,
+          height: 200.0,
+          decoration: new BoxDecoration(
+            color: _active? Colors.lightGreen[700] : Colors.grey[600],
+          ),
+        ),
       );
     }
 }
