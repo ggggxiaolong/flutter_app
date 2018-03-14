9a7defafc01fcf43649349aa02aa4b48e23f7306
mrtan <freemrtan@gmail.com>
2018-03-14 09:47:48 +0800

    混合模式的state管理

--- lib/main.dart
+++ lib/main.dart
@@ -25,47 +25,81 @@ class ParentWidget extends StatefulWidget {
 
 class _ParentWidgetState extends State<ParentWidget> {
   bool _active = false;
-   void _handleTapboxChanged(bool newValue){
-     setState((){
-       _active = newValue;
-     });
-   }
-  @override
-    Widget build(BuildContext context) {
-      return new TapBoxB(
-        active: _active,
-        onChange: _handleTapboxChanged,
-      );
-    }
 
-}
-
-class TapBoxB extends StatelessWidget {
-  TapBoxB({ Key key, this.active: false, this.onChange}) : super(key: key);
-  final bool active;
-  final ValueChanged<bool> onChange;
-
-  void _handleChange(){
-    onChange(!active);
+  void _handleTapboxChanged(bool newValue){
+    setState((){
+      _active = newValue;
+    });
   }
-
   @override
     Widget build(BuildContext context) {
-      return new GestureDetector(
-        onTap: _handleChange,
-        child: new Container(
-          child: new Center(
-            child: new Text(
-              active? 'Active': 'Inactive',
-              style: new TextStyle(fontSize: 32.0, color: Colors.white),
-            ),
-          ),
-          width: 200.0,
-          height: 200.0,
-          decoration: new BoxDecoration(
-            color: active ? Colors.lightGreen[700] : Colors.grey[600],
-          ),
+      return new Container(
+        child: new TapboxC(
+          active: _active,
+          onChanged: _handleTapboxChanged,
         ),
       );
     }
 }
+ class TapboxC extends StatefulWidget {
+   TapboxC({Key key, this.active: false, this.onChanged}) : super(key:key);
+
+   final bool active;
+   final ValueChanged<bool> onChanged;
+
+  @override
+    _TapboxCState createState() => new _TapboxCState();
+
+ }
+
+ class _TapboxCState extends State<TapboxC> {
+   bool _highlight = false;
+
+   void _handleTapDown(TapDownDetails details){
+     setState((){
+       _highlight = true;
+     });
+   }
+
+   void _handleTapUp(TapUpDetails details){
+     setState(() {
+       _highlight = false;
+     });
+   }
+
+   void _handleTapCancle(){
+     setState(() {
+       _highlight = false;
+     });
+   }
+
+   void _handleTap(){
+    widget.onChanged(!widget.active); 
+   }
+   @override
+     Widget build(BuildContext context) {
+       return new GestureDetector(
+         onTapDown: _handleTapDown,
+         onTapUp: _handleTapUp,
+         onTap: _handleTap,
+         onTapCancel: _handleTapCancle,
+         child: new Container(
+           child: new Center(
+             child: new Text(
+               widget.active ? 'Active' : 'Inactive',
+               style: new TextStyle(fontSize: 32.0, color: Colors.white),
+             ),
+           ),
+           width: 200.0,
+           height: 200.0,
+           decoration: new BoxDecoration(
+             color: widget.active? Colors.lightGreen[700] : Colors.grey[600],
+             border: _highlight ? new Border.all(
+               color: Colors.teal[700],
+               width: 10.0,
+             ): null,
+           ),
+         ),
+       );
+     }
+ }
