d64316faeb61ff1f6f970222d01908d36dc6624e
mrtan <freemrtan@gmail.com>
2018-03-14 22:15:25 +0800

    framework-basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Top Lakes'),
-        ),
-        body: new Center(
-          child: new ParentWidget(),
-        ),
-    ),);
-  }
-}
-
-class ParentWidget extends StatefulWidget {
-  @override
-    _ParentWidgetState createState() => new _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue){
-    setState((){
-      _active = newValue;
-    });
-  }
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+  final Widget title;
   @override
     Widget build(BuildContext context) {
       return new Container(
-        child: new TapboxC(
-          active: _active,
-          onChanged: _handleTapboxChanged,
+        height: 80.0,
+        decoration: new BoxDecoration(color: Colors.blue[500]),
+        padding: const EdgeInsets.fromLTRB(8.0, 24.0, 8.0, 0.0),
+        child: new Row(
+          children: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.menu),
+              tooltip: 'Navigation menu',
+              onPressed: null,
+            ),
+            new Expanded(
+              child: title,
+            ),
+            new IconButton(
+              icon: new Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            )
+          ],
         ),
       );
     }
 }
- class TapboxC extends StatefulWidget {
-   TapboxC({Key key, this.active: false, this.onChanged}) : super(key:key);
 
-   final bool active;
-   final ValueChanged<bool> onChanged;
-
+class MyScaffold extends StatelessWidget {
   @override
-    _TapboxCState createState() => new _TapboxCState();
-
- }
-
- class _TapboxCState extends State<TapboxC> {
-   bool _highlight = false;
-
-   void _handleTapDown(TapDownDetails details){
-     setState((){
-       _highlight = true;
-     });
-   }
-
-   void _handleTapUp(TapUpDetails details){
-     setState(() {
-       _highlight = false;
-     });
-   }
-
-   void _handleTapCancle(){
-     setState(() {
-       _highlight = false;
-     });
-   }
-
-   void _handleTap(){
-    widget.onChanged(!widget.active); 
-   }
-   @override
-     Widget build(BuildContext context) {
-       return new GestureDetector(
-         onTapDown: _handleTapDown,
-         onTapUp: _handleTapUp,
-         onTap: _handleTap,
-         onTapCancel: _handleTapCancle,
-         child: new Container(
-           child: new Center(
-             child: new Text(
-               widget.active ? 'Active' : 'Inactive',
-               style: new TextStyle(fontSize: 32.0, color: Colors.white),
-             ),
-           ),
-           width: 200.0,
-           height: 200.0,
-           decoration: new BoxDecoration(
-             color: widget.active? Colors.lightGreen[700] : Colors.grey[600],
-             border: _highlight ? new Border.all(
-               color: Colors.teal[700],
-               width: 10.0,
-             ): null,
-           ),
-         ),
-       );
-     }
- }
+    Widget build(BuildContext context) {
+      return new Material(
+        child: new Column(
+          children: <Widget>[
+            new MyAppBar(
+              title: new Text(
+                'Example title',
+                style: Theme.of(context).primaryTextTheme.title,
+              ),
+            ),
+            new Expanded(
+              child: new Center(
+                child: new Text('Hello, World'),
+              ),
+            )
+          ],
+        )
+      );
+    }
+}
