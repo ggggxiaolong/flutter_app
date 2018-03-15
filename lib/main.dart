bb94c5eee7a68c9ed80f189787e3f8c46d168cf3
mrtan <freemrtan@gmail.com>
2018-03-15 21:41:54 +0800

    framework-gestures

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,29 @@ class MyAppBar extends StatelessWidget {
     }
 }
 
+class MyButton extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      return new GestureDetector(
+        onTap: (){
+          print('MyButton was tapped!');
+        },
+        child: new Container(
+          height: 36.0,
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          decoration: new BoxDecoration(
+            borderRadius: new BorderRadius.circular(5.0),
+            color: Colors.lightGreen[500]
+          ),
+          child: new Center(
+            child: new Text('Engage'),
+          )
+        ),
+      );
+    }
+}
+
 class MyScaffold extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
@@ -44,11 +67,18 @@ class MyScaffold extends StatelessWidget {
             ),
             new Expanded(
               child: new Center(
-                child: new Text('Hello, World'),
+                child: new MyButton(),
               ),
             )
           ],
         )
       );
     }
+}
+
+void main(){
+  runApp(new MaterialApp(
+    title: 'My app',
+    home: new MyScaffold(),
+  ));
 }
