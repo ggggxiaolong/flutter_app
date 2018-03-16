64b9ccf65dd7081f7bf39ea96195d88f26b4537f
mrtan <freemrtan@gmail.com>
2018-03-16 14:09:08 +0800

    framework-shopitem

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,92 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    new MaterialApp(
-      title: 'Flutter Tutorial',
-      home: new TutorialHome(),
-    )
-  );
+class Product {
+  const Product({this.name});
+  final String name;
 }
 
-class TutorialHome extends StatelessWidget {
+typedef void CartCHangeCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
+    : product = product, super(key: new ObjectKey(product));
+  final Product product;
+  final bool inCart;
+  final CartCHangeCallback onCartChanged;
+
+  Color _getColor(BuildContext context){
+    return inCart ? Colors.black45 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context){
+    if(!inCart) return null;
+    return new TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+    Widget build(BuildContext context) {
+      return new ListTile(
+        onTap: (){ onCartChanged(product, !inCart);},
+        leading: new CircleAvatar(
+          backgroundColor: _getColor(context),
+          child: new Text(product.name[0]),
+        ),
+        title: new Text(product.name, style: _getTextStyle(context),),
+      );
+    }
+}
+
+class ShoppongList extends StatefulWidget {
+  ShoppongList({Key key, this.products}): super(key: key);
+  final List<Product> products;
+
+  @override
+    _ShoppingListState createState() => new _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppongList> {
+  Set<Product> _shoppingCart = new Set<Product>();
+
+  void _handleCartChange(Product product, bool inCart){
+    setState((){
+      if (inCart)
+        _shoppingCart.add(product);
+      else 
+        _shoppingCart.remove(product);
+    }); 
+  }
   @override
     Widget build(BuildContext context) {
       return new Scaffold(
         appBar: new AppBar(
-          leading: new IconButton(
-            icon: new Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          title: new Text('Example title'),
-          actions: <Widget>[
-            new IconButton(
-              icon: new Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            )
-          ],
+          title: new Text('Shopping List'),
         ),
-        body: new Center(
-          child: new Text('Hello World'),
-        ),
-        floatingActionButton: new FloatingActionButton(
-          tooltip: 'Add',
-          child: new Icon(Icons.add),
-          onPressed: null,
+        body: new ListView(
+          padding: new EdgeInsets.symmetric(vertical: 8.0),
+          children: widget.products.map((Product product){
+            return new ShoppingListItem(
+              product: product,
+              inCart: _shoppingCart.contains(product),
+              onCartChanged: _handleCartChange,
+            );
+          }).toList(),
         ),
       );
     }
 }
+
+ void main(){
+   runApp(new MaterialApp(
+     title: 'Shopping App',
+     home: new ShoppongList(
+       products: <Product>[
+         new Product(name: "Eggs"),
+         new Product(name: 'Flour'),
+         new Product(name: 'Chocolate chips'),
+       ],
+     ),
+   ));
+ }
