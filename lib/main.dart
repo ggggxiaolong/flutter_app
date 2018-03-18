b7df46ff4de2e2eeaab910803c7f01a38229b891
mrtan <freemrtan@gmail.com>
2018-03-18 17:18:49 +0800

    Gesture

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,48 @@
-import 'dart:async';
-import 'dart:isolate';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-import 'dart:convert';
 
 void main() {
-  runApp(new SampleApp());
+  runApp(new MaterialApp(
+    title: 'Fade in sample',
+    home: new SampleApp(),
+  ));
 }
 
-class SampleApp extends StatelessWidget {
-  @override
-    Widget build(BuildContext context) {
-      return new MaterialApp(
-        title: 'Sample App',
-        theme: new ThemeData(
-          primarySwatch: Colors.blue
-        ),
-        home: new SampleAppPage(),
-      );
-    }
+class SampleApp extends StatefulWidget{
+  @override _SampleAppState createState() => new _SampleAppState();
 }
 
-class SampleAppPage extends StatefulWidget {
-  SampleAppPage({Key key}): super(key: key);
-
-  @override
-    _SampleAppPageState createState() => new _SampleAppPageState();
-}
-
-class _SampleAppPageState extends State<SampleAppPage> {
-  List data = [];
-
-  bool _showLoadingDialog() {
-    if (data.isEmpty) {
-      return true;
-    }
-    return false;
-  }
-
-  Widget _getBody() {
-    if (_showLoadingDialog()) {
-      return new Center(
-        child: new CircularProgressIndicator(),
-      );
-    }
-    return new ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (BuildContext context, int position){
-            return _getRow(position);
-          },
-        );
-  }
+class _SampleAppState extends State<SampleApp> with TickerProviderStateMixin{
+  AnimationController controller ;
+  CurvedAnimation curve;
 
   @override
     void initState() {
       super.initState();
-      loadData();
+      controller = new AnimationController(duration: const Duration(milliseconds: 2000), vsync: this);
+      curve = new CurvedAnimation(parent: controller, curve: Curves.easeIn);
     }
 
   @override
     Widget build(BuildContext context) {
       return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Sample App'),
+        appBar: new AppBar(title: new Text("Fade in")),
+        body: new Center(
+          child: new GestureDetector(
+            child: new RotationTransition(
+              turns: curve,
+              child: new FlutterLogo(
+                size: 200.0,
+              ),
+            ),
+            onDoubleTap: (){
+              if (controller.isCompleted) {
+                controller.reverse();
+              } else {
+                controller.forward();
+              }
+            },
+          ),
         ),
-        body: _getBody()
       );
-    }
-
-    Widget _getRow(int position){
-      return new Padding(
-        padding: new EdgeInsets.all(10.0),
-        child: new Text('Row ${data[position]["title"]}'),
-      );
-    }
-
-    loadData() async {
-      String dataURL = "https://jsonplaceholder.typicode.com/posts";
-      ReceivePort receivePort = new ReceivePort();
-      await Isolate.spawn(dataLoader, receivePort.sendPort);
-      SendPort sendPort = await receivePort.first;
-
-      List msg = await sendReceive(sendPort, dataURL);
-
-      setState((){
-        data = msg;
-      });
-    }
-
-    static dataLoader(SendPort sendPort) async {
-      ReceivePort port = new ReceivePort();
-      sendPort.send(port.sendPort);
-
-      await for(var msg in port){
-        String data = msg[0];
-        SendPort replyTo = msg[1];
-        http.Response response = await http.get(data);
-        replyTo.send(JSON.decode(response.body));
-      }
-    }
-
-    Future sendReceive(SendPort port, msg){
-      ReceivePort response = new ReceivePort();
-      port.send([msg, response.sendPort]);
-      return response.first;
     }
 }
