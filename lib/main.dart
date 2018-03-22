f8c52a311fd92a5e688d465ae75f67e360d9ceba
mrTan <freemrtan@gmail.com>
2018-03-22 16:34:57 +0800

    cache image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(new ImageDemo());
 
@@ -12,8 +13,10 @@ class ImageDemo extends StatelessWidget {
           appBar: new AppBar(
             title: new Text('Web Image'),
           ),
-          body: new Image.network(
-            'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+          body: new CachedNetworkImage(
+            imageUrl: 'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+            placeholder: new Center(child: new CircularProgressIndicator()),
+            errorWidget: new Icon(Icons.error),
           ),
         ),
       );
