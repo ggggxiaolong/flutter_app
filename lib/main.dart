7ea47c03b267adb51c7488286d6459aa6363acb7
mrTan <freemrtan@gmail.com>
2018-03-22 15:07:24 +0800

    web image

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new TabBarDemo());
+void main() => runApp(new ImageDemo());
 
-class TabBarDemo extends StatelessWidget {
+class ImageDemo extends StatelessWidget {
 
   @override
     Widget build(BuildContext context) {
       return new MaterialApp(
         title: 'TabBar Demo',
-        home: new DefaultTabController(
-          length: 3,
-          child: new Scaffold(
-            appBar: new AppBar(
-              bottom: new TabBar(
-                tabs: <Widget>[
-                  new Tab(icon: new Icon(Icons.directions_car),),
-                  new Tab(icon: new Icon(Icons.directions_transit),),
-                  new Tab(icon: new Icon(Icons.directions_bike),)
-                ],
-              ),
-              title: new Text('Tabs Demo'),
-            ),
-            body: new TabBarView(
-              children: <Widget>[
-                new Icon(Icons.directions_car),
-                new Icon(Icons.directions_transit),
-                new Icon(Icons.directions_bike)
-              ],
-            ),
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Web Image'),
+          ),
+          body: new Image.network(
+            'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
           ),
         ),
       );
