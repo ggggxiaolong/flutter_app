b8716bc55eb48289ec8b8de2d3480f66d2b26ddd
mrtan <freemrtan@gmail.com>
2018-03-26 20:45:52 +0800

    Gesture tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new GrideApp());
-
-class GrideApp extends StatelessWidget {
-  @override
-    Widget build(BuildContext context) {
-      return new MaterialApp(
-        title: "Gride List",
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text("Gride App"),
-          ),
-          body: new GridView.count(
-            crossAxisCount: 2,
-            children: new List.generate(
-              100, 
-              (i) => new Center(
-                child: new Text(
-                  "Item $i",
-                  style: Theme.of(context).textTheme.headline,
-                ),
-              )
-            ),
-          ),
+void main() => runApp(new MaterialApp(
+    title: "Tap",
+    home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Gesture Demo"),
         ),
-      );
-    }
+        body: new TapApp())));
+
+class TapApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new GestureDetector(
+      onTap: () {
+        Scaffold
+            .of(context)
+            .showSnackBar(new SnackBar(content: new Text("Tap")));
+      },
+      child: new Container(
+        padding: new EdgeInsets.all(12.0),
+        decoration: new BoxDecoration(
+            color: Theme.of(context).buttonColor,
+            borderRadius: new BorderRadius.circular(8.0)),
+        child: new Text("My Button"),
+      ),
+    ));
+  }
 }
