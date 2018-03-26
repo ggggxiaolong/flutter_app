1bd5ed175e286396bbbb45c55852b7f71987bba9
mrtan <freemrtan@gmail.com>
2018-03-26 20:57:19 +0800

    InkWell

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
-    title: "Tap",
+    title: "InkWell",
     home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("Gesture Demo"),
+          title: new Text("InkWell Demo"),
         ),
         body: new TapApp())));
 
@@ -12,17 +12,15 @@ class TapApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Center(
-        child: new GestureDetector(
+        child: new InkWell(
       onTap: () {
+        Scaffold.of(context).removeCurrentSnackBar(reason: SnackBarClosedReason.remove);
         Scaffold
             .of(context)
             .showSnackBar(new SnackBar(content: new Text("Tap")));
       },
       child: new Container(
         padding: new EdgeInsets.all(12.0),
-        decoration: new BoxDecoration(
-            color: Theme.of(context).buttonColor,
-            borderRadius: new BorderRadius.circular(8.0)),
         child: new Text("My Button"),
       ),
     ));
