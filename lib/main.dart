65ef18787eabcad972404e897d0e397057518a43
mrtan <freemrtan@gmail.com>
2018-03-27 22:04:49 +0800

    Navigate to new screen and back

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 void main() => runApp(new MaterialApp(
     title: "Dismiss Items",
-    home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Dismissing Items"),
-        ),
-        body: new DismissApp())));
+    home:  new FirstScreen()));
 
-class DismissApp extends StatelessWidget {
-  final List<String> _items = new List<String>.generate(20,(i) => "Item $i");
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(
-      itemCount: _items.length,
-      itemBuilder: (context, index){
-        final item = _items[index];
-        return new Dismissible(
-          key: new Key(item),
-          onDismissed: (direction){
-            _items.removeAt(index);
-            Scaffold.of(context).removeCurrentSnackBar();
-            Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("$item dismissed"),));
-          },
-          background: new Container(color: Colors.red,),
-          child: new ListTile(title: new Text("$item"),),
-        );
-      },
-    );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("First Screen"),
+      ),
+      body: new Center(
+        child: new Center(
+          child: new RaisedButton(
+            child: new Text("Launch new Screen"),
+            onPressed: (){
+              Navigator.push(context, 
+              new MaterialPageRoute(builder: (context) => new SecondScreen()));
+            },
+          ),
+        ),
+      ),
+    ); 
   }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Second Screen"),
+        ),
+        body: new Center(
+          child: new RaisedButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: new Text("Go back!"),
+          ),
+        ),
+      );
+    }
 }
