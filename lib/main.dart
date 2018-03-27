efe0062633107e59e38894af9a59feacda02116f
mrtan <freemrtan@gmail.com>
2018-03-27 21:45:59 +0800

    Dismissing item

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 void main() => runApp(new MaterialApp(
-    title: "InkWell",
+    title: "Dismiss Items",
     home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("InkWell Demo"),
+          title: new Text("Dismissing Items"),
         ),
-        body: new TapApp())));
+        body: new DismissApp())));
 
-class TapApp extends StatelessWidget {
+class DismissApp extends StatelessWidget {
+  final List<String> _items = new List<String>.generate(20,(i) => "Item $i");
   @override
   Widget build(BuildContext context) {
-    return new Center(
-        child: new InkWell(
-      onTap: () {
-        Scaffold.of(context).removeCurrentSnackBar(reason: SnackBarClosedReason.remove);
-        Scaffold
-            .of(context)
-            .showSnackBar(new SnackBar(content: new Text("Tap")));
+    return new ListView.builder(
+      itemCount: _items.length,
+      itemBuilder: (context, index){
+        final item = _items[index];
+        return new Dismissible(
+          key: new Key(item),
+          onDismissed: (direction){
+            _items.removeAt(index);
+            Scaffold.of(context).removeCurrentSnackBar();
+            Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("$item dismissed"),));
+          },
+          background: new Container(color: Colors.red,),
+          child: new ListTile(title: new Text("$item"),),
+        );
       },
-      child: new Container(
-        padding: new EdgeInsets.all(12.0),
-        child: new Text("My Button"),
-      ),
-    ));
+    );
   }
 }
