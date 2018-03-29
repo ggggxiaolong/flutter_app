3717f4c2f796cf64bc0f4f8122270f0ca1c02fc8
mrtan <freemrtan@gmail.com>
2018-03-29 21:04:23 +0800

    websockets

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-import 'dart:async';
-import 'dart:convert';
-
-class Post {
-  final int userId;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.userId, this.id, this.title, this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return new Post(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
-  }
-}
-
-Future<Post> fetchPost() async {
-  final response =
-      await http.get('https://jsonplaceholder.typicode.com/posts/1');
-  final json = JSON.decode(response.body);
-  return new Post.fromJson(json);
-}
+import 'package:web_socket_channel/io.dart';
+import 'package:flutter/foundation.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
+  final title = "WebSocket Demo";
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Fetch Data Example',
-      theme: new ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Fetch Data Example'),
+    Widget build(BuildContext context) {
+      return new MaterialApp(
+        title: title,
+        home: new MyHomePage(
+          title: title,
+          channel: new IOWebSocketChannel.connect("ws://echo.websocket.org"),
         ),
-        body: new Center(
-          child: new FutureBuilder<Post>(
-            future: fetchPost(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return new Text(snapshot.data.title);
-              } else if (snapshot.hasError) {
-                return new Text("${snapshot.error}");
-              }
-              return new CircularProgressIndicator();
-            },
+      );
+    }
+}
+
+class MyHomePage extends StatefulWidget {
+  final title;
+  final WebSocketChannel channel;
+  MyHomePage({Key key, this.title, this.channel}):super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _controller = new TextEditingController();
+  @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text(widget.title),
+        ),
+        body: new Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Form(
+                child: new TextFormField(
+                  controller: _controller,
+                  decoration: new InputDecoration(labelText: "Send a message"),
+                ),
+              ),
+              new StreamBuilder<String>(
+                stream: widget.channel.stream,
+                builder: (context, snapshot){
+                  return new Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24.0),
+                    child: new Text(snapshot.hasData ? "${snapshot.data}" : ''),
+                  );
+                },
+              ),
+            ],
           ),
         ),
-      ),
-    );
-  }
+        floatingActionButton: new FloatingActionButton(
+          onPressed: _sendMessage,
+          tooltip: "Send message",
+          child: new Icon(Icons.send),
+        ),
+      );
+    }
+
+    void _sendMessage() {
+      if (_controller.text.isNotEmpty) {
+        widget.channel.sink.add(_controller.text);
+        _controller.clear();
+      }
+    }
+
+    @override
+      void dispose() {
+        widget.channel.sink.close();
+        super.dispose();
+      }
 }
