7ec8fd0586dda013f552fd7be7191601c162f717
mrtan <freemrtan@gmail.com>
2018-03-29 19:47:17 +0800

    transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
-      title: "Return date",
-      home: new HomeScreen(),
+      title: "Transition Demo",
+      home: new MainScreen(),
     ));
 
-class HomeScreen extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Returning Data Demo"),
-      ),
-      body: new SelectionButton(),
-    );
-  }
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Main Screen"),
+        ),
+        body: new GestureDetector(
+          child: new Hero(
+            tag: "imageHero",
+            child: new Image.network(
+              'https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg',
+            ),
+          ),
+          onTap: (){
+            Navigator.push(
+              context,
+              new MaterialPageRoute(
+                builder: (context) => new DetailScreen(),
+              ));
+          },
+        ),
+      );
+    }
 }
 
-class SelectionButton extends StatelessWidget {
+class DetailScreen extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
-    return new Center(
-        child: new RaisedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: new Text("Pick an option, any option"),
-    ));
-  }
-
-  _navigateAndDisplaySelection(BuildContext context) async {
-    final result = await Navigator.push(context,
-        new MaterialPageRoute(builder: (context) => new SelectionScreen()));
-
-    Scaffold.of(context).showSnackBar(new SnackBar(
-          content: new Text("$result"),
-        ));
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Pick an option"),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: new RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: new Text("Yep!"),
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        body: new GestureDetector(
+          onTap: (){
+            Navigator.pop(context);
+          },
+          child: new Center(
+            child: new Hero(
+              tag: "imageHero",
+              child: new Image.network(
+                'https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg',
               ),
             ),
-            new Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: new RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context, "Nope!");
-                },
-                child: new Text('Nope!'),
-              ),
-            )
-          ],
+          ),
         ),
-      ),
-    );
-  }
+      );
+    }
 }
